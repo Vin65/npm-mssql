207600af19f6b38602c98e21353473f37bee0357
lucasklaassen <lucasklaassen1@gmail.com>
2018-03-27 08:24:34 -0700

    Adding logging for connection pool

--- dist/objects/MssqlClient.js
+++ dist/objects/MssqlClient.js
@@ -16,20 +16,26 @@ var MssqlClient = function () {
   _createClass(MssqlClient, [{
     key: 'execute',
     value: function execute(query, params) {
+      console.log('Create MSSQL Connection Pool');
       return new mssql.ConnectionPool(this.dbConfig.toString()).connect().then(function (pool) {
+        console.log('Grab MSSQL pool request.');
         pool = pool.request();
         for (var key in params) {
           pool = pool.input(key, params[key]);
         }
+        console.log('Run query on MSSQL Pool.');
         pool = pool.query(query);
         return pool;
       }).then(function (queryResults) {
+        console.log('MSSQL Success, close connection pool.');
         mssql.close();
         return queryResults;
       }).catch(function (error) {
+        console.log('MSSQL Fail, close connection pool.');
         mssql.close();
         throw error;
       }).finally(function () {
+        console.log('MSSQL finally close connection pool.');
         mssql.close();
       });
     }
