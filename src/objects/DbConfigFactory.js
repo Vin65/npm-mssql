b710cf208772a4ab3c0891ac68a8c09d56e17a85
lucasklaassen <lucasklaassen1@gmail.com>
2018-03-27 19:55:26 -0700

    test

--- src/objects/DbConfigFactory.js
+++ src/objects/DbConfigFactory.js
@@ -5,7 +5,7 @@ const ProductionDbConfig = require('./ProductionDbConfig');
 
 class DbConfigFactory {
   static build(environment, dbConfig) {
-    console.log('ENV!@#$%^&', environment)
+    console.log('ENV!@#$%^&', JSON.stringify(environment))
     let klass = (environment === 'production') ? ProductionDbConfig : StagingDbConfig;
 
     return new klass(dbConfig);
