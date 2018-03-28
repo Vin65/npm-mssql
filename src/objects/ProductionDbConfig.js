4d295cf7231adc12651c8bda2d9769aafadaa682
lucasklaassen <lucasklaassen1@gmail.com>
2018-03-28 07:55:39 -0700

    Remove console logs

--- src/objects/ProductionDbConfig.js
+++ src/objects/ProductionDbConfig.js
@@ -7,8 +7,6 @@ class ProductionDbConfig {
   constructor(dbConfig) {
     this.dbConfig = dbConfig;
     this.configs = {};
-    console.log('dbconfig.database', this.dbConfig.database)
-    console.log('dbconfig.server', this.dbConfig.server)
     this._addConfigs(
       [{
         dataSource: this.dbConfig.database,
@@ -22,23 +20,18 @@ class ProductionDbConfig {
     config.database = database.toLowerCase();
     config.server = server;
 
-    console.log('config', JSON.stringify(config));
 
     return config;
   }
 
   _addConfigs(datasources) {
-    console.log(datasources, JSON.stringify(datasources))
     datasources.forEach((datasource) => {
-      console.log(`datasource: ${datasource} config`, JSON.stringify(this._config(datasource.dataSource, datasource.host)));
       this.configs[datasource.dataSource.toLowerCase()] = this._config(datasource.dataSource, datasource.host);
     });
   }
 
   async _addConfigsPromise(datasources) {
-    console.log(datasources, JSON.stringify(datasources))
     datasources.forEach((datasource) => {
-      console.log(`datasource: ${datasource} config`, JSON.stringify(this._config(datasource.dataSource, datasource.host)));
       this.configs[datasource.dataSource.toLowerCase()] = this._config(datasource.dataSource, datasource.host);
     });
   }
@@ -46,27 +39,20 @@ class ProductionDbConfig {
   _fetchDatasources() {
     let mssqlClient = new MssqlClient(this.dbConfig);
     return mssqlClient.execute(MssqlQuery.select.datasource()).then(queryResults => {
-      console.log('fetchedDatasources', JSON.stringify(queryResults));
       if (queryResults.recordsets[0].length) {
-        console.log('datasources found!!!', queryResults.recordsets[0]);
         return queryResults.recordsets[0];
       }
-      console.log('datasources not found...');
     }).catch(error => {
       console.error(error);
     });
   }
 
   async for_(database) {
-    console.log('for', database)
     let config = this.configs[database.toLowerCase()];
-    console.log('for_ config 1#:', JSON.stringify(config))
     if (!config) {
-      console.log('for_ config 2#:');
       await this._addConfigsPromise(await this._fetchDatasources());
     }
 
-    console.log('for_ config 3#:', JSON.stringify(this.configs[database.toLowerCase()]))
 
     return this.configs[database.toLowerCase()];
   }
